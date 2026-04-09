cb1d50785e8645395b89fff7c54f675fd2450a91
WiktorJJ <jankowski@insa-toulouse.fr>
2026-04-09 12:10:26 +0200

    adding a library with the QR code

---
+++ Arduino Code/QR.h
@@ -0,0 +1,13 @@
+/*
+  QR.h -Library which contains the binary strucutre for a QR.png that links to the Github page of the developers
+  Created by Wiktor Jankowski and Jaon Solé, on April 9, 2026 
+*/
+#ifndef QR_h
+#define QR_h
+
+#include "Arduino.h"
+
+//contains the QR code for the github of project @WiktorJJ and @Joan
+extern const unsigned char epd_bitmap_QR_code_github [] PROGMEM
+
+#endif
